41c0aaad74f919bd15fde000c8c0cf7fe9ed3361
James McCoy <jamessan@jamessan.com>
2023-11-29 18:18:07 -0500

    Display all dependencies if "-a" is used
    Signed-off-by: James McCoy <jamessan@jamessan.com>

--- src/tree.rs
+++ src/tree.rs
@@ -208,7 +208,7 @@ fn print_package<'a>(
 
     println!("{}", format.display(package));
 
-    if !package.show_dependencies() {
+    if !all && !package.show_dependencies() {
         return;
     }
 
